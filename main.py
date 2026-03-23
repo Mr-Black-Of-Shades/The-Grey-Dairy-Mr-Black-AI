81b20397e4f940e7dcc4689cd251e8c3eb7f99dd
By Shiladitya Mallick <info.shiladityamallick@gmail.com>
2026-03-24 00:49:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
 
 from config import BOT_TOKEN
-from handlers import start
+from handlers import start, handle_next
 from callbacks import handle_buttons
+
 
 def main():
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+
+    # ✅ NEXT button handler
+    app.add_handler(CallbackQueryHandler(handle_next, pattern="^next$"))
+
+    # ✅ other buttons (pay, micro etc.)
     app.add_handler(CallbackQueryHandler(handle_buttons))
 
     print("Bot running...")
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
