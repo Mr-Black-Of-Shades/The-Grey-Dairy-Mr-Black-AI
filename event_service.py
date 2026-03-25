e09ff384420d4b813daf91235809a8b154d65685
By Shiladitya Mallick <info.shiladityamallick@gmail.com>
2026-03-25 18:46:04 +0530

    Update event_service.py

--- event_service.py
+++ event_service.py
@@ -1,13 +1,19 @@
-from supabase_client import supabase
+from db import get_cursor
+import json
 
 
 def track_event(user_id, event_type, data=None):
 
     try:
-        supabase.table("events").insert({
-            "user_id": user_id,
-            "event_type": event_type,
-            "event_data": data or {}
-        }).execute()
+        cur = get_cursor()
+
+        cur.execute(
+            """
+            INSERT INTO events (user_id, event_type, event_data)
+            VALUES (%s, %s, %s)
+            """,
+            (user_id, event_type, json.dumps(data or {}))
+        )
+
     except Exception as e:
         print("EVENT ERROR:", e)
