bf9a953f2748e6f54355b96c3a67a0115bf9c8c2
By Shiladitya Mallick <info.shiladityamallick@gmail.com>
2026-03-25 18:56:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
